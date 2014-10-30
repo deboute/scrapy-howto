697625ca6e0b7ab9ae46047dfd41830b964b4b55
Benjamin Debouté <benjamin@deboute.fr>
2014-10-30 18:37:35 +0100

    adding setup using pbr

---
+++ podtags/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# -*- python -*-
+# use 4 spaces to indent, NO TAB
+# vim: ai ts=4 sts=4 et sw=4
+ 
+import setuptools
+setuptools.setup(
+    setup_requires=['pbr'],
+    pbr=True)
