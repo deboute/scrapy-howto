6ad2d7d13e0805688462718f283ed2abd932f212
Benjamin Debouté <benjamin@deboute.fr>
2014-11-01 14:15:59 +0100

    refactoring test_spiders

--- podtags/tests/test_spiders.py
+++ podtags/tests/test_spiders.py
@@ -5,18 +5,25 @@
 """
 Test spiders using local webserver
 """
+#
+# IMPORT
+#
+# BASE
 import subprocess
 import unittest
 import os
-
+# SCRAPY
 from scrapy.crawler import Crawler
 from scrapy.utils.project import get_project_settings
-from twisted.internet import reactor, task
 from scrapy import log, signals
+# TWISTED
+from twisted.internet import reactor, task
+# CUSTOM
 from podtags.spiders.podbay import PodbaySpiderForGames
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
- 
+
+# launch local  web server
 html_server = subprocess.Popen([
     'python',
     os.path.join(
@@ -27,39 +34,63 @@ html_server = subprocess.Popen([
     )
 ])
  
-def setup_crawler():
-    items = [] 
-    spider = TestSpider()
+def setup_crawler(
+        spider_class,
+        **kwargs
+    ):
+    """
+    Use scrapy in a script
+    see http://doc.scrapy.org/en/latest/topics/practices.html
+
+    :param spider_class: Spider class to test
+    :type spider_class: text
+    """
+    items = []
+    def add_item(item):
+        items.append(item)
+    # create Crawler
     settings = get_project_settings()
     crawler = Crawler(settings)
     crawler.configure()
+    # connect collecting function on item_passed
     crawler.signals.connect(add_item, signals.item_passed)
+    # create & connect spider
+    spider = spider_class(**kwargs)
     crawler.crawl(spider)
-    #log.start()
+    # start crawler
+    log.start()
     crawler.start()
- 
-def add_item(item):
-    global items
-    items.append(item)
- 
-class TestSpider(PodbaySpiderForGames):
-    allowed_domains = ['localhost']
-    start_urls = [
-        'http://localhost:8080/browse/games-and-hobbies'
-    ]
+    # run crawler
+    task.deferLater(reactor, 1, reactor.stop)
+    reactor.run()
+    return items
 
 class SpidersTests(unittest.TestCase):
+    """
+    Test case for Spiders
+    """
     def test_PodbaySpiderForGames(self):
-        global items
-        items = []
+        """
+        Test PodbaySpiderForGames
+        """
         try:
-            setup_crawler()
-            task.deferLater(reactor, 1, reactor.stop)
-            reactor.run()
+            # create custom Crawler
+            # and fetch generated items
+            items = setup_crawler(
+                spider_class=PodbaySpiderForGames,
+                allowed_domains = ['localhost'],
+                start_urls = [
+                    'http://localhost:8080/browse/games-and-hobbies'
+                ]
+            )
+            # execute test cases
             self.assertEqual(len(items), 1)
             self.assertEqual(items[0]['id'], '123456789')
         except:
+            # end custom html server
+            # and raise Exception
             html_server.kill()
             raise
         finally:
+            # end custom html server
             html_server.kill()
